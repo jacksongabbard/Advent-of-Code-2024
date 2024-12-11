91c29c7d6818a041e8ce3ee7746ccaae7ea28ca6
Jackson Gabbard <jg@jg.gg>
2024-12-11 11:48:51 +0000

    Day 11

---
+++ Day-11/Part-1.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+
+const input =
+  fs.readFileSync('./input.txt')
+    .toString('utf-8')
+    .replace('\n', '')
+    .split(' ');
+
+function node(val) {
+  return { val, next: undefined };
+}
+
+function makeLinkedList(input) {
+  if (input.length === 0) {
+    return undefined;
+  }
+  let linkedList = node(input[0]);
+  let tail = linkedList;
+  for (let i=1; i<input.length; i++) {
+    tail.next = node(input[i]);
+    tail = tail.next;
+  }
+
+  return linkedList;
+}
+
+function print(ll) {
+  let t = ll;
+  let out = '';
+  while (t) {
+    out += t.val + ' ';
+    t = t.next;
+  }
+  console.log(out);
+}
+
+
+function blink(ll) {
+  let curr = ll;
+  while (curr) {
+    if (curr.val === '0') {
+      curr.val = '1';
+    } else if (curr.val.length % 2 === 0) {
+      const v = curr.val;
+      const left = v.substring(0, v.length / 2);
+      const right = parseInt(v.substring(v.length / 2), 10).toString();
+      curr.val = left;
+      const temp = curr.next;
+      curr.next = node(right);
+      curr.next.next = temp;
+      curr = curr.next;
+    } else {
+      curr.val = (parseInt(curr.val, 10) * 2024).toString();
+    }
+    curr = curr.next;
+  }
+
+  // print(ll);
+}
+
+const ll = makeLinkedList(input);
+// print(ll);
+for (let i=0; i<75; i++) {
+  blink(ll);
+}
+
+let stones = 0;
+let curr = ll;
+while (curr) {
+  stones++;
+  curr = curr.next;
+}
+
+console.log({ stones });
